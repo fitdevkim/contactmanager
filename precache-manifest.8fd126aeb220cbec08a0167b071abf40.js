976d47f3ccffb3fe4b3d32ca841a3599f8b0430c
Muhammad Nurhakim Bin Margono <hakimzdope@gmail,com>
2019-04-20 13:48:40 +0800

    Updates

--- precache-manifest.8fd126aeb220cbec08a0167b071abf40.js
+++ precache-manifest.8fd126aeb220cbec08a0167b071abf40.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "1e3c13ba422e7e692c38",
+    "revision": "80f867415eb9eb0522eb",
     "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
   },
   {
-    "revision": "1e3c13ba422e7e692c38",
-    "url": "/contactmanager/static/js/main.1e3c13ba.chunk.js"
+    "revision": "80f867415eb9eb0522eb",
+    "url": "/contactmanager/static/js/main.80f86741.chunk.js"
   },
   {
     "revision": "e4bf40f27ed8dbd6f9da",
     "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
   },
   {
-    "revision": "0b1502f7f2191d8b5b47",
+    "revision": "d475116e89fd3452f77a",
     "url": "/contactmanager/static/css/2.93ea2c4e.chunk.css"
   },
   {
-    "revision": "0b1502f7f2191d8b5b47",
-    "url": "/contactmanager/static/js/2.0b1502f7.chunk.js"
+    "revision": "d475116e89fd3452f77a",
+    "url": "/contactmanager/static/js/2.d475116e.chunk.js"
   },
   {
-    "revision": "1ddbd55c99525f99c193ba53e47da86d",
+    "revision": "82f3dc25ac82da139aa1ccb737cf1f41",
     "url": "/contactmanager/index.html"
   }
 ];
